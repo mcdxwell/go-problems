5d9dd357f736f7975cc3bffa3e42869e31cdb069
mcdxwell <itsdavzz@gmail.com>
2022-05-03 23:10:10 -0500

    Recursive binary search

--- binarysearch/main.go
+++ binarysearch/main.go
@@ -5,10 +5,9 @@ import "fmt"
 func IterBSearch(arr []int, l, r, x int) int {
 	m := 0
 	for l <= r {
-		fmt.Println(l, r)
+
 		m = l + (r-l)/2
-		fmt.Println("index", m)
-		fmt.Println("arr[m] is ", arr[m])
+
 		if arr[m] == x {
 			return m
 		}
@@ -24,7 +23,23 @@ func IterBSearch(arr []int, l, r, x int) int {
 }
 
 func RecurBSearch(arr []int, l, r, x int) int {
-	return 1
+	m := 0
+	if r >= l {
+
+		m = l + (r-l)/2
+
+		if arr[m] == x {
+			return m
+		}
+
+		if arr[m] > x {
+			return RecurBSearch(arr, l, m-1, x)
+		}
+
+		return RecurBSearch(arr, m+1, r, x)
+	}
+
+	return -1
 }
 
 func main() {
@@ -35,5 +50,7 @@ func main() {
 	l := 0
 	r := n - 1
 	result := IterBSearch(arr, l, r, x)
+	fmt.Println(result)
+	result = RecurBSearch(arr, l, r, x)
 	fmt.Println(result)
 }
