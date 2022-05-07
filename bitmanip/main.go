0cdb24b3dcc0d4e99768a0ce0e3091494a12d1f9
mcdxwell <itsdavzz@gmail.com>
2022-05-06 21:03:31 -0500

    More bitmanip

--- bitmanip/main.go
+++ bitmanip/main.go
@@ -36,7 +36,24 @@ func (c *client) IsConnected() bool {
 	}
 }
 
-func main() {
+func swap(a, b int) (int, int) {
+	a ^= b
+	b ^= a
+	a ^= b
+	return a, b
+}
+
+func otherswap(a, b int) (int, int) {
+	a, b = b, a
+	return a, b
+}
+
+func genericswap[T any](a, b T) (any, any) {
+	a, b = b, a
+	return a, b
+}
+
+func flipBits() {
 	var n byte = 0x0F
 	fmt.Printf("%016b\n", n)
 	n = ^n
@@ -45,8 +62,11 @@ func main() {
 
 	fmt.Printf("Len16(%016b) = %d\n", 8, bits.Len16(8))
 	fmt.Printf("Len16(%016b) = %d\n", m, bits.Len16(m))
+}
 
-	client1 := client{status: connected}
+func main() {
+
+	/* client1 := client{status: connected}
 	client2 := client{status: disconnected}
 
 	fmt.Println(client1.IsConnected())
@@ -55,6 +75,14 @@ func main() {
 	client1.status = disconnected
 	fmt.Println(client1.IsConnected())
 	client1.status = connected
-	fmt.Println(client1.IsConnected())
+	fmt.Println(client1.IsConnected()) */
+	fmt.Println(swap(10, 12))
+	fmt.Println(swap(10000, 100))
+	fmt.Println(otherswap(10, 12))
+	fmt.Println(genericswap(10, 12))
+	fmt.Println(genericswap("hello", "good bye"))
+	var n byte = 0x0F
+	fmt.Println(n)
+	fmt.Println(genericswap(n, n))
 
 }
